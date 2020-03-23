1abdca158e13a642194a955361f02bc4c954230e
Murali Krishna <vmurali1444@gmail.com>
2020-03-23 22:10:57 +0100

    :sparkles: add success, failure methods for slack notofication

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -1,15 +1,33 @@
-def sendMessage(hook, message) {
-    echo "Hook: $hook and Message: $message"
+import groovy.transform.Field
+
+@Field private final String ERROR_COLOR = "#f72c2c"
+@Field private final String SUCCESS_COLOR = "#36a64f"
+
+@Field
+private final String ERROR_MESSAGE = ":fire: Failed big time... :fire:"
+
+@Field
+private final String SUCCESS_MESSAGE = ":rocket: BOOM!! Now you are live... :rocket:"
+
+def failureMessage(String hook) {
+    sendMessage(hook, ERROR_COLOR, ERROR_MESSAGE)
+}
+
+def successMessage(String hook) {
+    sendMessage(hook, SUCCESS_COLOR, SUCCESS_MESSAGE)
+}
+
+def sendMessage(String hook, String color, String message) {
     sh """curl -X POST \
             $hook \
             -d '{
                 "attachments": [
                     {
-                       "color": "#f72c2c",
-                       "author_name": "BASE_IMAGE - Failed",
+                       "color": "$color",
+                       "author_name": "Build ${currentBuild.result}",
                        "title": "Build No: ${currentBuild.number}",
                        "title_link": "${currentBuild.absoluteUrl}",
-                       "text": ":fire: Failed big time... :fire:",
+                       "text": "$message - ${currentBuild.changeSets}",
                     }
                 ]
             }'
