a3e3daabc3d28ff97e523593c0d2f48544b2df43
Murali Krishna <vmurali1444@gmail.com>
2020-03-26 20:19:13 +0100

    :sparkles: slack - make previousBuild status null safe

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -27,7 +27,7 @@ def sendFixedMessage(String hook) {
                         "author_name": "${env.JOB_BASE_NAME} - ${currentBuild.result}",
                         "title": "Build: ${currentBuild.displayName}",
                         "title_link": "${currentBuild.absoluteUrl}",
-                        "text": "ufff... :green_heart: Previous(${currentBuild.previousBuild.displayName}) build got fixed now :green_heart:"
+                        "text": "ufff... :green_heart: Previous(${currentBuild.previousBuild?.displayName}) build got fixed now :green_heart:"
                     }
                 ]
             }'
